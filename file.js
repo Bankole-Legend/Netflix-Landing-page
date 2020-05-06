9c0f5fd161b050c56763ff7bd8b9330a8d95176c
Bankole_Adefemi <bankoledavidadefemi@gmail.com>
2020-05-06 23:58:46 +0100

    additions to javascript

--- file.js
+++ file.js
@@ -1,19 +1,28 @@
 // Grabing all the icons
-const boxes = document.querySelectorAll('tab-item');
-const tabContent = document.querySelectorAll('tab-content-item');
+const boxes = document.querySelectorAll('.tab-item');
+const tabContent = document.querySelectorAll('.tab-content-item');
 
-function showItem(){
-    removeBorder()
-    this.classList.add('tab-border')
+// This function occurs on click
+function showItem(e){
+    removeBorder();
+    display();
+    this.classList.add('tab-border');
+    
+    const showing = document.querySelector(`#${this.id}-content`);
+    showing.classList.add('show');
+    
 }
 
+// The function to remove the red border
 function removeBorder() {
-    boxes.forEach(box => box.classList.remove('tab-border'))
+    boxes.forEach(box => box.classList.remove('tab-border'));
 }
-function removeBorder() {
-    tabContent.forEach(box => box.classList.remove('show'))
+// The function to remove the display content
+function display() {
+    tabContent.forEach(box => box.classList.remove('show'));
 }
 
 
-boxes.forEach(item => item.addEventListener('click', showItem))
+
+boxes.forEach(box => box.addEventListener('click', showItem));
 
